fafe72e792ac9c9a5bbdf6de32f918c5b2b449aa
Anders Roos <anders.roos@gmail.com>
2016-01-06 22:44:02 +0100

    Finding a good acceleration algorithm.

--- acceleration.py
+++ acceleration.py
@@ -5,7 +5,20 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-def true_accel(steps, a):
+# Algorithm 1, delay:
+#
+# d0 = sqrt(1/a) ???
+# dn = d0(sqrt(n + 1) - sqrt(n))
+#
+# Algorithm 2, taylor approx delay:
+#
+# d0 = constant based on a
+# dn = dn-1 * ( 1 - 2 / (4n + 1)) = dn-1 * (4n - 1) / (4n + 1)
+# inversern är:
+# dn-1 = dn * (4n + 1) / (4n - 1)
+
+
+def accel(steps, a):
     
     df = pd.DataFrame(index=np.arange(0, steps), columns=('v', 's', 'd', 't'))
 
@@ -19,15 +32,64 @@ def true_accel(steps, a):
         df.loc[s] = [v, s, 1/v, t]
     return df.dropna()
 
-df = true_accel(1500, 1000)
+def accel_1(steps, a):
 
-dft = df.set_index('t')
-dft.plot(kind='line', subplots=True, layout=(1, 3), figsize=(18, 4), ylim=(0, None))
-plt.show()
-
-dfs = df.set_index('s')
-dfs.plot(kind='line', subplots=True, layout=(1, 3), figsize=(18, 4), ylim=(0, None))
-plt.show()
-
+    df = pd.DataFrame(index=np.arange(0, steps), columns=('v', 's', 'd', 't'))
     
+    t = 0.0
+    d0 = d = math.sqrt(1/a)
+    
+    df.loc[0] = [0, 0, 0, 0];
+    for s in np.arange(1, steps):
+        t = t + d
+        df.loc[s] = [1/d, s, d, t]
+        d = d0 * (math.sqrt(s + 1) - math.sqrt(s))
+    return df.dropna()
+
+def accel_2(steps, a):
+
+    df = pd.DataFrame(index=np.arange(0, steps), columns=('v', 's', 'd', 't'))
+    
+    t = 0.0
+    d0 = d = math.sqrt(1/a)
+    
+    df.loc[0] = [0, 0, 0, 0];
+    for s in np.arange(1, steps):
+        t = t + d
+        df.loc[s] = [1/d, s, d, t]
+        if s < 500:
+            d = d * (1 - 2 / (4 * s + 1))
+        else:
+            u = 1000 - s
+            d = d * (4 * u + 1) / (4 * u -1 )
+            if d < 0:
+                break
+
+    return df.dropna()
+    
+a = 10000 # steps / s²
+    
+df0 = accel(1500, a)
+df1 = accel_1(1500, a)
+df2 = accel_2(1500, a)
+
+ax = df0[['t', 'd']].set_index('t').plot(kind='line', ylim=(0, None))
+df1[['t', 'd']].set_index('t').plot(kind='line', ylim=(0, None), ax=ax)
+df2[['t', 'd']].set_index('t').plot(kind='line', ylim=(0, None), ax=ax)
+plt.show()
+
+# dft = df.set_index('t')
+# dft.plot(kind='line', subplots=True, layout=(2, 3), figsize=(18, 4), ylim=(0, None))
+# plt.show()
+# 
+# dfs = df.set_index('s')
+# dfs.plot(kind='line', subplots=True, layout=(2, 3), figsize=(18, 4), ylim=(0, None))
+# plt.show()
+
+# Algo måste kunna prognostisera inbromsningar (target kan ändras när
+# som helst), men jag tror man kan räkna från speed faktiskt.
+# Utföra decelaration på samma sätt som acceleration (alltid ligga på samma kurva, ingen förändring vid max).
+# Beräkning vara tillräckligt snabb.
+# Mikrostega.
+# Bra kurva i start och slut.
     
---
+++ timecomp.cpp
@@ -0,0 +1,55 @@
+#include "Arduino.h"
+
+uint32_t n = 123489853;
+uint32_t t = 1343542;
+
+void setup()
+{ 
+   Serial.begin(9600);
+}
+
+uint32_t t0;
+uint32_t t1;
+float sum = 0;
+uint32_t count;
+
+#define START 9000
+
+void loop()
+{
+   count = 0;
+   sum = 0;
+
+   // float division => 48 us => 768 cycles
+   // float multiplication => 27 us => 432 cycles
+   // float sum => 24 us => 384 cycles
+   // uint32 multiplication => ~1 us
+   // uint32 sum => ~1 us
+   // uint32 division => 38 us => 608 cycles
+   // uint32 multiplication => ~1 us
+   // uint32 sum => ~1 us
+   // uint32 count in loop, aka measurment error => 0.001 us (which is a lie because less than clock cycle)
+   // uint16 division => 13 us => 208 cycles
+   // uint16 multiplication => ~1 us
+   // uint8  division => ~5 us => 80 cycles
+   // uint8  multiplication => ~1 us
+   
+   
+   t0 = micros();
+   for (float x = START + 1; x < START + 1e3; ++x) {
+      for (float y = START + 1; y < START + 1e3; ++y) {
+         sum += x + y;
+         count++;
+      }
+   }
+   t1 = micros();
+   
+   Serial.print("time: ");
+   Serial.print(t1 - t0);
+   Serial.print(" count: ");
+   Serial.print(count);
+   Serial.print(" avg: ");
+   Serial.print((t1 - t0) / count);
+   Serial.print(" sum: ");
+   Serial.println(sum);
+}
